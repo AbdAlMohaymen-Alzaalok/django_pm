3c452b2af908222c1ea270715a4b5e9cb1435b8d
AbdAlMohaymen-Alzaalok <mohiamenazaaloc@gmail.com>
2024-08-15 16:02:55 +0300

    Create and List projects

--- projects/urls.py
+++ projects/urls.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from django.urls import path,include
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('',views.Projectlistview.as_view(), name='Project_list'),
+    path('project/create',views.ProjectCreateView.as_view(), name='Project_create')
+]
